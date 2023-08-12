38752e2e39ecc1f1565252fffbe20f782a2480aa
khmalz <akmalrma1710@gmail.com>
2023-08-12 08:05:21 +0700

    Add some form in register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\RedirectResponse;
+use App\Models\Passenger;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
-use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Auth\Events\Registered;
+use App\Providers\RouteServiceProvider;
 
 class RegisteredUserController extends Controller
 {
@@ -32,8 +33,12 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'gender' => ['required', 'string'],
+            'telephone' =>  ['required', 'numeric', 'regex:/^(\62|0*)[2-9]{1}[0-9]{5,20}$/']
+        ], [
+            'telephone.regex' => "The phone number must start with 62/0"
         ]);
 
         $user = User::create([
@@ -42,8 +47,14 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        Passenger::create([
+            'user_id' => $user->id,
+            'telephone' => $request->telephone,
+            'gender' => $request->gender,
+        ]);
+
         $user->assignRole('Penumpang');
-        
+
         event(new Registered($user));
 
         Auth::login($user);
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,15 +10,17 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('/admin/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ asset('/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- BS Stepper -->
+    <link rel="stylesheet" href="{{ asset('admin/plugins/bs-stepper/css/bs-stepper.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('/admin/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/dist/css/adminlte.min.css') }}">
 </head>
 
 <body class="hold-transition register-page">
-    <div class="register-box">
+    <div class="register-box" style="width: 500px !important">
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
                 <a href="{{ route('home') }}" class="h1"><b>{{ config('app.name') }}</b> Apps</a>
@@ -26,7 +28,133 @@
             <div class="card-body">
                 <p class="login-box-msg">Register a new account</p>
 
-                <form action="{{ route('register') }}" method="post">
+                <div class="bs-stepper">
+                    <div class="bs-stepper-header" role="tablist">
+                        <!-- your steps here -->
+                        <div class="step" data-target="#account-part">
+                            <button type="button" class="step-trigger" role="tab" aria-controls="account-part"
+                                id="account-part-trigger">
+                                <span class="bs-stepper-circle">1</span>
+                                <span class="bs-stepper-label">Account</span>
+                            </button>
+                        </div>
+                        <div class="line"></div>
+                        <div class="step" data-target="#information-part">
+                            <button type="button" class="step-trigger" role="tab" aria-controls="information-part"
+                                id="information-part-trigger">
+                                <span class="bs-stepper-circle">2</span>
+                                <span class="bs-stepper-label">Information</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="bs-stepper-content">
+                        <!-- your steps content here -->
+                        <form action="{{ route('register') }}" method="post">
+                            @csrf
+                            <div id="account-part" class="content" role="tabpanel"
+                                aria-labelledby="account-part-trigger">
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                        placeholder="Full name" name="name" value="{{ old('name') }}" autofocus>
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-user"></span>
+                                        </div>
+                                    </div>
+                                    @error('name')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                        placeholder="Email" name="email" value="{{ old('email') }}">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-envelope"></span>
+                                        </div>
+                                    </div>
+                                    @error('email')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                        placeholder="Password" name="password">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-lock"></span>
+                                        </div>
+                                    </div>
+                                    @error('password')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <input type="password" class="form-control" placeholder="Retype password"
+                                        name="password_confirmation">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-lock"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-primary" onclick="stepper.next()">Next</button>
+                            </div>
+                            <div id="information-part" class="content" role="tabpanel"
+                                aria-labelledby="information-part-trigger">
+                                <div class="form-group">
+                                    <label for="inputTelephone">Telephone</label>
+                                    <div class="input-group mb-3">
+                                        <input type="text"
+                                            class="form-control @error('telephone') is-invalid @enderror"
+                                            placeholder="No Telp" name="telephone" value="{{ old('telephone') }}"
+                                            autofocus>
+                                        <div class="input-group-append">
+                                            <div class="input-group-text">
+                                                <span class="fas fa-phone"></span>
+                                            </div>
+                                        </div>
+                                        @error('telephone')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Jenis Kelamin</label>
+                                    <select class="form-control @error('gender') is-invalid @enderror" name="gender">
+                                        <option disabled selected>Pilih</option>
+                                        <option {{ old('gender') == 'L' ? 'selected' : null }} value="L">
+                                            Laki-laki
+                                        </option>
+                                        <option {{ old('gender') == 'P' ? 'selected' : null }} value="P">
+                                            Perempuan
+                                        </option>
+                                    </select>
+                                    @error('gender')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                                <button type="button" class="btn btn-primary"
+                                    onclick="stepper.previous()">Previous</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                {{-- <form action="{{ route('register') }}" method="post">
                     @csrf
                     <div class="input-group mb-3">
                         <input type="text" class="form-control @error('name') is-invalid @enderror"
@@ -90,7 +218,7 @@
                         </div>
                         <!-- /.col -->
                     </div>
-                </form>
+                </form> --}}
 
                 <a href="{{ route('login') }}" class="text-center">I already have a account</a>
             </div>
@@ -100,11 +228,20 @@
     <!-- /.register-box -->
 
     <!-- jQuery -->
-    <script src="{{ asset('/admin/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('/admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- BS-Stepper -->
+    <script src="{{ asset('admin/plugins/bs-stepper/js/bs-stepper.min.js') }}"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('/admin/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('admin/dist/js/adminlte.min.js') }}"></script>
+
+    <script>
+        // BS-Stepper Init
+        document.addEventListener('DOMContentLoaded', function() {
+            window.stepper = new Stepper(document.querySelector('.bs-stepper'))
+        })
+    </script>
 </body>
 
 </html>
